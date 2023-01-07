bee064cbd14207c754558f604e35d41fa8f65306
Igor Ignatov <kantorawb@gmail.com>
2023-01-07 13:52:05 +0400

    Домашнее задание

--- newApp/ViewController.swift
+++ newApp/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var container: UIView!
+    @IBOutlet weak var skyImageView: UIImageView!
+    @IBOutlet weak var Button: UIButton!
+    
+    private var isSun: Bool = false
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    @IBAction func buttonDidTapDown(_ sender: Any) {
+        print("> buttonDidTapDown")
+        
+        skyImageView.tintColor = .blue
+    }
+    
+    @IBAction func buttonDidTap(_ sender: Any) {
+        if isSun {
+            skyImageView.image = UIImage(systemName: "sun.max")
+            skyImageView.tintColor = .orange
+            container.backgroundColor = .white
+            Button.tintColor = .black
 
-
+        } else {
+            skyImageView.image = UIImage(systemName: "moon")
+            skyImageView.tintColor = .yellow
+            container.backgroundColor = .black
+            Button.tintColor = .white
+        }
+        
+        isSun.toggle()
+    }
 }
 
