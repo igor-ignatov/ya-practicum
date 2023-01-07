a98b58785347d718f4c4424c10a702bcca448fc1
Igor Ignatov <kantorawb@gmail.com>
2023-01-07 13:04:11 +0400

    Initial Commit

---
+++ newApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newApp
+//
+//  Created by Igor Ignatov on 07.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
