dab4ac83e8a12faa26fb0bb976722492db91e20c
Igor Ignatov <kantorawb@gmail.com>
2023-01-07 14:10:52 +0400

    Домашнее задание по спринту 3

--- newApp/ViewController.swift
+++ newApp/ViewController.swift
@@ -8,37 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var container: UIView!
-    @IBOutlet weak var skyImageView: UIImageView!
-    @IBOutlet weak var Button: UIButton!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var countButton: UIButton!
     
-    private var isSun: Bool = false
-        
+    private var counterPrefix = "Значение счётчика:"
+    private var count: Int = 0 {
+        didSet {
+            countLabel.text = "\(counterPrefix) \(count)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    @IBAction func buttonDidTapDown(_ sender: Any) {
-        print("> buttonDidTapDown")
         
-        skyImageView.tintColor = .blue
+        countLabel.text = "\(counterPrefix) \(count)"
     }
-    
-    @IBAction func buttonDidTap(_ sender: Any) {
-        if isSun {
-            skyImageView.image = UIImage(systemName: "sun.max")
-            skyImageView.tintColor = .orange
-            container.backgroundColor = .white
-            Button.tintColor = .black
 
-        } else {
-            skyImageView.image = UIImage(systemName: "moon")
-            skyImageView.tintColor = .yellow
-            container.backgroundColor = .black
-            Button.tintColor = .white
-        }
-        
-        isSun.toggle()
+    @IBAction func buttonDidTap(_ sender: Any) {
+        count += 1
     }
 }
 
